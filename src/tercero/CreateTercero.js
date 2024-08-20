d85fa5c1728d9f6559c67ed466f8ebca0c785532
RICK <rickmartinezbanda@gmail.com>
2024-08-20 12:01:29 -0500

    actualizacion style2

--- src/tercero/CreateTercero.js
+++ src/tercero/CreateTercero.js
@@ -96,17 +96,6 @@ const CompCreateTercero = () => {
       <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong" data-bs-theme="dark">
                     <div className='container-fluid justify-content-center'>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <ul className="navbar-nav mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link to="/showTercero" className="nav-link fs-4">Lista</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/create" className="nav-link fs-4">Formulario</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/" className="nav-link fs-4">Inicio</Link>
-                                </li>
-                            </ul>
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className='collapse navbar-collapse justify-content-center' id="navbarSupportedContent">
