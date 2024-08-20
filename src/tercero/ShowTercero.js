f5e3436777cd8e3f15419b0a4961972e75f8d4bc
RICK <rickmartinezbanda@gmail.com>
2024-08-20 11:45:48 -0500

    actualizacion style

--- src/tercero/ShowTercero.js
+++ src/tercero/ShowTercero.js
@@ -33,23 +33,26 @@ const CompShowTercero = () => {
     return (
         <div className="full-page-background">
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong" data-bs-theme="dark">
-                    <div className='container-fluid justify-content-center'>
-                        <div className='collapse navbar-collapse justify-content-center' id="navbarSupportedContent">
-                            <ul className="navbar-nav mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link to="/showTercero" className="nav-link fs-4">Lista</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/create" className="nav-link fs-4">Formulario</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/" className="nav-link fs-4">Inicio</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+            <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong" data-bs-theme="dark">
+            <div className='container-fluid justify-content-center'>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className='collapse navbar-collapse justify-content-center' id="navbarSupportedContent">
+                    <ul className="navbar-nav mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to="/showTercero" className="nav-link fs-4">Lista</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/create" className="nav-link fs-4">Formulario</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link fs-4">Inicio</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
             </div>
             <br />
             <br />
