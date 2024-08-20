38bc89cd9d8e83aa82ac729c8e41aa0339623dca
RICK <rickmartinezbanda@gmail.com>
2024-08-20 13:52:07 -0500

    actualizacion styles7

--- src/tercero/ShowTercero.js
+++ src/tercero/ShowTercero.js
@@ -29,30 +29,30 @@ const CompShowTercero = () => {
             console.error('Error al eliminar el tercero:', error);
         }
     };
-    
+
     return (
         <div className="full-page-background">
             <div>
-            <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong" data-bs-theme="dark">
-    <div className='container-fluid'>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className='collapse navbar-collapse justify-content-center' id="navbarSupportedContent">
-            <ul className="navbar-nav mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <Link to="/showTercero" className="nav-link fs-4">Lista</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/create" className="nav-link fs-4">Formulario</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/" className="nav-link fs-4">Inicio</Link>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+                <nav className="navbar navbar-expand-lg navbar-dark shadow-5-strong" data-bs-theme="dark">
+                    <div className='container-fluid'>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className='collapse navbar-collapse justify-content-center' id="navbarSupportedContent">
+                            <ul className="navbar-nav mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link to="/showTercero" className="nav-link fs-4">Lista</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/create" className="nav-link fs-4">Formulario</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/" className="nav-link fs-4">Inicio</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
             <br />
             <br />
@@ -61,26 +61,26 @@ const CompShowTercero = () => {
                 <div className="d-flex flex-wrap justify-content-center">
                     {tercero.map((tercero) => (
                         <div className="card mb-4 mx-2 bg-dark text-white" style={{ width: '24rem' }} key={tercero.id}>
-                        <div className="card-header">
-                            <h5 className="card-title">Registro</h5>
-                        </div>
-                        <div className="card-body">
-                            <h6 className="card-text mb-2">Identificación: <small>{tercero.identificacion}</small></h6>
-                            <h6 className="card-text mb-2">Tipo de persona: <small>{tercero.tipoPersona}</small></h6>
-                            <h6 className="card-text mb-2">Dígito verificación: <small>{tercero.digitoVerificacion}</small></h6>
-                            <h6 className="card-text mb-2">Nombre representante legal: <small>{tercero.nombreRepresentanteLegal}</small></h6>
-                            <h6 className="card-text mb-2">Tipo de actividad: <small>{tercero.tipoActividad}</small></h6>
-                            <h6 className="card-text mb-2">Tipo cliente: <small>{tercero.tipoCliente}</small></h6>
-                            <div className="d-flex justify-content-center">
-                                <Link to={`/edit/${tercero.id}`} className="btn btn-black mb-2 me-2 btn-large">
-                                    <i className="fa-solid fa-pen-to-square"></i>
-                                </Link>
-                                <button onClick={() => deleteTercero(tercero.id)} className="btn btn-red mb-2 btn-large">
-                                    <i className="fa-solid fa-trash"></i>
-                                </button>
+                            <div className="card-header">
+                                <h5 className="card-title">Registro</h5>
+                            </div>
+                            <div className="card-body">
+                                <h6 className="card-text mb-2">Identificación: <small>{tercero.identificacion}</small></h6>
+                                <h6 className="card-text mb-2">Tipo de persona: <small>{tercero.tipoPersona}</small></h6>
+                                <h6 className="card-text mb-2">Dígito verificación: <small>{tercero.digitoVerificacion}</small></h6>
+                                <h6 className="card-text mb-2">Nombre representante legal: <small>{tercero.nombreRepresentanteLegal}</small></h6>
+                                <h6 className="card-text mb-2">Tipo de actividad: <small>{tercero.tipoActividad}</small></h6>
+                                <h6 className="card-text mb-2">Tipo cliente: <small>{tercero.tipoCliente}</small></h6>
+                                <div className="d-flex justify-content-center">
+                                    <Link to={`/edit/${tercero.id}`} className="btn btn-light mb-2 me-2 btn-large">
+                                        <i className="fa-solid fa-pen-to-square"></i>
+                                    </Link>
+                                    <button onClick={() => deleteTercero(tercero.id)} className="btn btn-danger mb-2 btn-large">
+                                        <i className="fa-solid fa-trash"></i>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     ))}
                 </div>
             </div>
